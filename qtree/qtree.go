007ab88a493005b6f3a5c4816c42469a475ab9b8
Christian Stewart <christian@paral.in>
2017-02-01 18:39:55 -0500

    feat: add/remove child to qtree

--- qtree/qtree.go
+++ qtree/qtree.go
@@ -1,11 +1,15 @@
 package qtree
 
 import (
-// proto "github.com/rgraphql/rgraphql/pkg/proto"
+	"errors"
+	"fmt"
+
+	proto "github.com/rgraphql/rgraphql/pkg/proto"
 )
 
 type QueryTreeNode struct {
-	Id uint32
+	Id        uint32
+	idCounter uint32
 
 	Root     *QueryTreeNode
 	Parent   *QueryTreeNode
@@ -28,4 +32,87 @@ func NewQueryTree() *QueryTreeNode {
 	}
 	nqt.Root = nqt
 	return nqt
+}
+
+// Apply a tree mutation to the tree.
+func (qt *QueryTreeNode) ApplyTreeMutation(aqn *proto.RGQLTreeMutation) error {
+	// Find the node we are operating on.
+	nod, ok := qt.Root.RootNodeMap[aqn.NodeId]
+	if !ok {
+		return fmt.Errorf("Unable to find target node %d.", aqn.NodeId)
+	}
+
+	switch aqn.Operation {
+	case proto.RGQLTreeMutation_SUBTREE_ADD_CHILD:
+		return nod.AddChild(aqn.Node)
+	case proto.RGQLTreeMutation_SUBTREE_DELETE:
+		if aqn.NodeId == 0 || nod == qt.Root {
+			return errors.New("You cannot delete the root node.")
+		}
+		nod.Dispose()
+		return nil
+	default:
+		return fmt.Errorf("Unknown mutation type %v", aqn.Operation)
+	}
+}
+
+// AddChild validates and adds a child tree.
+func (qt *QueryTreeNode) AddChild(data *proto.RGQLQueryTreeNode) (addChildErr error) {
+	if _, ok := qt.RootNodeMap[data.Id]; ok {
+		return fmt.Errorf("Invalid node ID (already exists): %d", data.Id)
+	}
+
+	// Mint the new node.
+	// TODO: handle arguments, field name, etc.
+	nnod := &QueryTreeNode{
+		Id:     data.Id,
+		Parent: qt,
+		Root:   qt.Root,
+	}
+	qt.Children = append(qt.Children, nnod)
+
+	// Early failout cleanup defer.
+	defer func() {
+		if addChildErr != nil {
+			qt.removeChild(nnod)
+		}
+	}()
+
+	// Apply any children
+	for _, child := range data.Children {
+		if err := nnod.AddChild(child); err != nil {
+			return err
+		}
+	}
+
+	// Apply to the resolver tree (start resolution for this node).
+	// NOTE: just do this on the topmost newly applied leaf in the tree.
+	return nil
+}
+
+// removeChild deletes the given child from the children array.
+func (qt *QueryTreeNode) removeChild(nod *QueryTreeNode) {
+	for i, item := range qt.Children {
+		if item == nod {
+			a := qt.Children
+			copy(a[i:], a[i+1:])
+			a[len(a)-1] = nil
+			qt.Children = a[:len(a)-1]
+			break
+		}
+	}
+}
+
+// Dispose deletes the node and all children.
+func (qt *QueryTreeNode) Dispose() {
+	for _, child := range qt.Children {
+		child.Dispose()
+	}
+	qt.Children = nil
+	if qt.Root != nil && qt.Root.RootNodeMap != nil {
+		delete(qt.Root.RootNodeMap, qt.Id)
+	}
+	if qt.Parent != nil {
+		qt.Parent.removeChild(qt)
+	}
 }
