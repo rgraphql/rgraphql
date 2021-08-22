d3af4a59e7ff941089038688bd6bb15003f819eb
Christian Stewart <christian@paral.in>
2021-08-22 14:24:29 -0700

    refactor: update rollup configuration
    Signed-off-by: Christian Stewart <christian@paral.in>

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,8 +1,7 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import json from '@rollup/plugin-json'
 
 const pkg = require('./package.json')
 
@@ -22,19 +21,16 @@ export default {
     // Allow json resolution
     json(),
     // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript(),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
+    // https://github.com/rollup/@rollup/plugin-node-resolve#usage
     resolve({
       namedExports: {
         'graphql': ['visit'],
       },
     }),
-
-    // Resolve source maps to the original source
-    sourceMaps(),
   ],
 }
