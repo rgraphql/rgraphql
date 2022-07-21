6282ae19c2fe25e5284861460b94434f29d872c7
Christian Stewart <christian@paral.in>
2022-07-21 02:23:16 -0700

    fix: import correct rgraphql import
    Signed-off-by: Christian Stewart <christian@paral.in>

--- src/query-tree/query-tree-handler.ts
+++ src/query-tree/query-tree-handler.ts
@@ -1,8 +1,8 @@
-import { rgraphql } from 'rgraphql'
+import * as rgraphql from 'rgraphql'
 import { QueryTreeNode } from './query-tree-node'
 
 // QueryTreeHandler handles changes to the query tree.
-export type QueryTreeHandler = (mut: rgraphql.IRGQLQueryTreeMutation) => void
+export type QueryTreeHandler = (mut: rgraphql.RGQLQueryTreeMutation) => void
 
 // QueryTreePurgeHandler handles query nodes being purged from the query tree.
 export type QueryNodePurgeHandler = (qnode: QueryTreeNode) => void
--- src/result-tree/result-tree-handler.ts
+++ src/result-tree/result-tree-handler.ts
@@ -1,6 +1,6 @@
-import { rgraphql } from 'rgraphql'
+import * as rgraphql from 'rgraphql'
 
 // HandleResultValue handles the next value in the sequence, optionally
 // returning a handler for the next value(s) in the sequence.
 // If val == undefined, delete the value.
-export type ResultTreeHandler = ((val: rgraphql.IRGQLValue | undefined) => ResultTreeHandler) | null
+export type ResultTreeHandler = ((val: rgraphql.RGQLValue | undefined) => ResultTreeHandler) | null
